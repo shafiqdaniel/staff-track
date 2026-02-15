01c8ce89ceccff34720f3806b0573a27ca957bd4
shafiqdaniel <shafiqdaniel247@gmail.com>
2026-02-16 00:53:20 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Centralized Firebase Configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyCOxGlMtNCPXfd8_vAuIBfUgz1Fsn_Tr7Q",
+    authDomain: "stafftrack-16a87.firebaseapp.com",
+    projectId: "stafftrack-16a87",
+    storageBucket: "stafftrack-16a87.firebasestorage.app",
+    messagingSenderId: "555632662062",
+    appId: "1:555632662062:web:7dd32f29027ff248e631bf"
+};
